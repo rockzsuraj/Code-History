adb6c641735f8aae5bbb6925aca97c4114630000
skumarz11 <surajkmr012@gmail.com>
2023-02-02 19:46:41 +0530

    added main page for quiz app

--- Code History/ContentView.swift
+++ Code History/ContentView.swift
@@ -8,14 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    let mainColor = Color(red: 20/255, green: 28/255,
+                          blue: 58/255)
+    let accentColor = Color(red: 48/255, green: 105/255, blue: 240/255)
+    let white = Color(red: 1, green: 1, blue: 1)
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            mainColor.ignoresSafeArea()
+            VStack {
+                Text("1/10").font(.callout).multilineTextAlignment(.trailing).padding()
+                Text("What was the first computer bug?").font(.largeTitle).bold().multilineTextAlignment(.leading)
+                Spacer()
+                HStack {
+                    Button(action: {
+                        print("Tapped on Choice 1")
+                    }, label: {Text("Ant").font(.body).multilineTextAlignment(.center).padding().border(accentColor, width: 4)})
+                    Button(action: {print("Tapped on Choice 2")},
+                    label: {
+                        Text("Bettle").font(.body).multilineTextAlignment(.center).padding().border(accentColor, width: 4)
+                    })
+                    Button(action: {print("Tapped on Choice 3")},
+                    label: {
+                        Text("Moth").font(.body).multilineTextAlignment(.center).padding().border(accentColor, width: 4)
+                    })
+                    Button(action: {
+                        print("Tapped on Choice 4")
+                    },
+                    label: {
+                        Text("Fly").font(.body).multilineTextAlignment(.center).padding().border(accentColor, width: 4)
+                    })
+                }
+            }.foregroundColor(.white)
         }
-        .padding()
     }
 }
 
